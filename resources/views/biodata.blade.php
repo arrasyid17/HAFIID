42912eb273e716a218b61c311e49a431aa4c9622
arrasyid17 <alhafiidar@gmail.com>
2024-03-21 03:58:56 +0700

    menambahkan foto pada biodata

--- resources/views/biodata.blade.php
+++ resources/views/biodata.blade.php
@@ -3,8 +3,13 @@
 <div class="w-full px-6 py-6 mx-auto">
   <div class="flex flex-wrap -mx-3">
     <div class="w-full px-3">
-      <h1 class="text-3xl font-bold mb-4">Halaman Biodata</h1>
-      <p class="text-lg leading-relaxed">Informasi biodata Anda dapat ditambahkan di sini.</p>
+      <h1 class="text-3xl font-bold mb-4">Biodata</h1>
+      <p class="text-lg leading-relaxed">Ini adalah biodata ku</p>
+      <p class="text-lg leading-relaxed">Nama: Al-Hafiid Ar-Rasyid</p>
+      <p class="text-lg leading-relaxed">Tempat, Tanggal Lahir: Pontianak, 17 April 2004</p>
+      <p class="text-lg leading-relaxed">Pendidikan: Prodi Sistem Informasi Universitas Tanjungpura</p>
+      <p class="text-lg leading-relaxed">Hobi: Olahraga</p>
+      <img src="{{ asset('assets/img/fotosaya.jpeg') }}" alt="Foto Profil" class="mt-4 w-24 h-24" style="width: 150px; height: 150px;">
     </div>
   </div>
 </div>
